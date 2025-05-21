eea20a293cc10d08fc1d5de121a5eab17d9c7941
Noah Choi <noah.choi0713@gmail.com>
2025-05-21 23:05:25 +0900

    [refactor/#18] 탐지 종료시간 설정
    - 로직 수정 중 2차 커밋
    - 5초간 탐지 후 결과 페이지가 아닌 탐지 시작 화면으로 넘어가는 오류 발생

--- app/src/main/java/com/example/allinsafe_spoofing/Ac5_02_spoofingdetect_process.kt
+++ app/src/main/java/com/example/allinsafe_spoofing/Ac5_02_spoofingdetect_process.kt
@@ -98,7 +98,7 @@ class Ac5_02_spoofingdetect_process : ComponentActivity() {
 
         //실제 스푸핑 코드와 연동하였을때 사용하기 위한 함수
         Toast.makeText(this.applicationContext,"스푸핑 탐지가 완료되었습니다!",Toast.LENGTH_LONG).show()
-        var intent=Intent(this,Ac5_03_spoofoingdetect_completed::class.java)
+        var intent=Intent(this,Ac5_03_spoofingdetect_completed::class.java)
         startActivity(intent)
     }
 }
